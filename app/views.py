d648394e7f99f4102cebdc654fe5e3206eb3ec0e
Sriram Thiagarajan <sri.sjc@gmail.com>
2016-08-18 16:09:40 +0530

    simple file upload

--- app/views.py
+++ app/views.py
@@ -1,4 +1,5 @@
 from app import app
+from flask import request, render_template
 
 @app.route('/')
 def index():
@@ -15,3 +16,13 @@ def show_user_profile(username):
 @app.route('/post/<int:post_id>/')
 def show_post(post_id):
     return 'Post %d' % post_id
+
+@app.route('/upload/', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'GET':
+        return render_template('upload_form.html')
+    if request.method == 'POST':
+        f = request.files['my_file']
+        print "Uploaded file name - %s" % f.filename
+        f.save('/tmp/my_file.txt');
+        return 'File saved successfully'

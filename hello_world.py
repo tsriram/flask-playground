e53ff17130972ad0e793709b634b36adf4ea5656
Sriram Thiagarajan <sri.sjc@gmail.com>
2016-08-18 12:32:08 +0530

    hello world

---
+++ hello_world.py
@@ -0,0 +1,18 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Index page'
+
+@app.route('/hello')
+def hello_world():
+    return 'Hello, World!'
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    return "User %s" % username
+
+@app.route('/post/<int:post_id>/')
+def show_post(post_id):
+    return 'Post %d' % post_id
